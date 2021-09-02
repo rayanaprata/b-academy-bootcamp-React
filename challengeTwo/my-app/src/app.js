3255ceacdb1d71b5e0fd461b0c5e9e2e68001cdb
Rayana Prata <rayanaprata@uol.com.br>
2021-09-02 16:17:38 -0300

    challengeTwo - exercise01

--- challengeTwo/my-app/src/app.js
+++ challengeTwo/my-app/src/app.js
@@ -5,12 +5,41 @@ import { Sidebar } from "./components/Sidebar/index";
 import { Contents } from "./components/Contents/index";
 import { Footer } from "./components/Footer/index";
 
+const articles = [
+  {
+    id: 1,
+    title: 'Clean Architecture on Frontend', // https://dev.to/bespoyasov/clean-architecture-on-frontend-4311
+    content: (
+      <p>
+        What is clean architecture in general and get familiar with 
+        such concepts as domain, use case and application layers.
+      </p>
+    )
+  },
+  {
+    id: 2,
+    title: 'React Practice Project for Beginner to Advance', // https://dev.to/undefinedhere/react-practice-project-for-beginner-to-advance-18da
+    content: (
+      <p>Many developers or beginners just learn all the fundamentals concepts 
+        but they don't implement those concepts. 
+        So Projects are one of the best ways to implement those concepts.</p>
+    )
+  },
+  {
+    id: 3,
+    title: 'How I structure my React projects', // https://dev.to/larswaechter/how-i-structure-my-react-projects-jii
+    content: (
+      <p>Architecture of React: What should the folder structure look like?</p>
+    )
+  },
+]
+
 function App() {
   return (
     <div className="container">
       <Header />
       <Menu />
-      <Sidebar />
+      <Sidebar articles={articles} />
       <Contents />
       <Footer />
     </div>
--- challengeTwo/my-app/src/components/Sidebar/index.js
+++ challengeTwo/my-app/src/components/Sidebar/index.js
@@ -1,5 +1,18 @@
 import "./index.css";
 
-export function Sidebar() {
-    return <aside>Sidebar</aside>;
+export function Sidebar(props) {
+
+    console.log(props);
+
+    return (
+        <aside>
+            <ul className="links-sidebar">
+                {props.articles.map((item) => (
+                    <li key={item.id}>
+                        <a className="link-sidebar" href="/"> - {item.title}</a>
+                    </li>
+                ))}
+            </ul> 
+        </aside>
+    );
 }
